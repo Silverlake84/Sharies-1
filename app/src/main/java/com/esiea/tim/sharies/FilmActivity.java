e835e6906735f939f57eff3cb4ba7c3b580055ee
COLIN tim <32955194+COLINtim@users.noreply.github.com>
2018-05-25 16:28:37 +0200

    ajout telechargement

--- app/src/main/java/com/esiea/tim/sharies/FilmActivity.java
+++ app/src/main/java/com/esiea/tim/sharies/FilmActivity.java
@@ -1,8 +1,10 @@
 package com.esiea.tim.sharies;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
@@ -16,6 +18,17 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class FilmActivity extends AppCompatActivity {
 
     private String id;
@@ -28,10 +41,11 @@ public class FilmActivity extends AppCompatActivity {
         double vote_average = getIntent().getExtras().getDouble("vote_average");
 
         String title = getIntent().getExtras().getString("title");
-        String poster_path = getIntent().getExtras().getString("poster_path");
         String overview = getIntent().getExtras().getString("overview");
         String release_date = getIntent().getExtras().getString("release_date");
-        String backdrop_path = getIntent().getExtras().getString("backdrop_path");
+
+        final String poster_path = getIntent().getExtras().getString("poster_path");
+        final String backdrop_path = getIntent().getExtras().getString("backdrop_path");
 
         this.id = String.valueOf(getIntent().getExtras().getInt("id"));
 
@@ -60,6 +74,21 @@ public class FilmActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                Uri url = Uri.parse("https://image.tmdb.org/t/p/w500"+poster_path);
+                DownloadManager.Request r1 = new DownloadManager.Request(url);
+                r1.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "poster-Red_Sparrow");
+
+            }
+        });
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri url = Uri.parse("https://image.tmdb.org/t/p/w500"+backdrop_path);
+                DownloadManager.Request r1 = new DownloadManager.Request(url);
+                r1.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "bg-Red_Sparrow");
+
             }
         });
     }
@@ -85,5 +114,22 @@ public class FilmActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void copyInputStreamToFile(InputStream in, File file){
+        Log.d("test", "copyInputStreamToFile: debut");
+        try{
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        Log.d("test", "copyInputStreamToFile: fin");
     }
 }
